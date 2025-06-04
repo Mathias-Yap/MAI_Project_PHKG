6ba228335090e03e4852c6f0ee689fca04e2ad40
V1nc3ntG1t <vincent.hoogstraten.2003@gmail.com>
2025-06-04 14:13:17 +0200

    test qeustions

--- tests/test_concept_retrieval.py
+++ tests/test_concept_retrieval.py
@@ -16,7 +16,35 @@ with open(file, "r") as f:
 vector_store = VectorStore()
 
 # Test the vector store with each question
-def test_concept_retrieval():
+def test_concept_retrieval_vector_store_alone():
+    results = []
+
+    for question_id in questions.keys():
+        expected_classes =  questions[question_id]['classes']
+        expected_predicates = questions[question_id]['object_properties']
+
+        tik = time.time()
+
+        # Prediction Step
+        found_classes, found_predicates  = vector_store.query(questions[question_id]["question"])
+
+        # Calculate the results
+        tok = time.time()
+        results.append(
+            {
+                "question": question_id,
+                "found_classes": found_classes,
+                "found_predicates": found_predicates,
+                "expected_classes": expected_classes,
+                "expected_predicates": expected_predicates,
+                "recall": len(set(found_classes + found_predicates).intersection(set(expected_classes + expected_predicates))) / len(expected_classes + expected_predicates),
+                "precision": len(set(found_classes + found_predicates).intersection(set(expected_classes + expected_predicates))) / len(found_classes + found_predicates) if (found_classes + found_predicates) else 1,
+                "time": tik - tok,
+            }
+        )
+        
+# Test the vector store and connecting with each question
+def test_concept_retrieval_with_connecting():
     results = []
 
     for question_id in questions.keys():
@@ -46,7 +74,5 @@ def test_concept_retrieval():
                 "time": tik - tok,
             }
         )
-        
-
 
 
